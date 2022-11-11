64db17d3e1bae4d93c76aa1a227caa5d5002af0a
Elena <elena1709@yandex.ru>
2022-11-11 13:19:36 +0300

    commit for hw

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов
+
+int[,] GetInt2dArray(int rows, int columns, int maxValue, int minValue)
+{
+    int[,] Random2dArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Random2dArray[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return Random2dArray;
+}
+
+void Print2dArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NumberOfRowsMinSum(int[,] array)
+{
+    int tempSum;
+    int minSum = 0;
+    int result = 0;
+    for (int column = 0; column < array.GetLength(1); column++)
+    {
+        minSum += array[0, column];
+    }
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        tempSum = 0;
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            tempSum += array[row, column];
+        }
+        if (tempSum < minSum)
+        {
+            minSum = tempSum;
+            result = row;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов: {result + 1}");
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное случайное число: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное случайное число: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetInt2dArray(rows, columns, maxValue, minValue);
+Print2dArray(array);
+Console.WriteLine();
+NumberOfRowsMinSum(array);
